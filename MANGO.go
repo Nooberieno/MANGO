050c2e9d9eb470176d6fc76a3f4f15a0fc2f330f
Nooberieno <nooberieno.dev@gmail.com>
2024-06-02 12:58:30 +0200

    argument testing

--- MANGO.go
+++ MANGO.go
@@ -1,1 +1,16 @@
-package mango
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide an argument")
+		return
+	}
+	for i, arg := range os.Args[1:] {
+		fmt.Printf("Argument %d: %s\n", i+1, arg)
+	}
+}
