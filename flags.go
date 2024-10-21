4e4dbda544378ab5dd49a726b44492f36be7d61a
Nooberieno <nooberieno.dev@gmail.com>
2024-10-21 15:36:35 +0200

    simplified usage message when suplying -h or --help, made switch case easier to read

--- flags.go
+++ flags.go
@@ -14,11 +14,11 @@ var (
 )
 
 func init() {
-	flag.BoolVar(&verbose, "v", false, "Enable verbose output")
+	flag.BoolVar(&verbose, "v", false, "")
 	flag.BoolVar(&verbose, "verbose", false, "Enable verbose output")
-	flag.BoolVar(&shell, "s", false, "Enable shell command execution")
+	flag.BoolVar(&shell, "s", false, "")
 	flag.BoolVar(&shell, "shell", false, "Enable shell command execution")
-	flag.BoolVar(&quiet, "q", false, "Enable quiet output")
+	flag.BoolVar(&quiet, "q", false, "")
 	flag.BoolVar(&quiet, "quiet", false, "Enable quiet output")
 }
 
@@ -26,18 +26,14 @@ func parse_flags() {
 	for _, arg := range os.Args[1:] {
 		if strings.HasPrefix(arg, "-") {
 			switch arg {
-			case ("-verbose"):
-				log.Fatal("Please use --verbose or -v to enable verbose output")
-			case ("-shell"):
-				log.Fatal("Please use --shel or -s to enable shell command execution")
-			case ("--s"):
-				log.Fatal("Please use --shel or -s to enable shell command execution")
-			case ("--v"):
-				log.Fatal("Please use --verbose or -v to enable verbose output")
-			case ("--q"):
-				log.Fatal("Please use --quiet or -q to enable quiet output")
-			case ("-quiet"):
-				log.Fatal("Please use --quiet or -q to enable quiet output")
+			case "-shell", "--s":
+				log.Fatal("Please use --shel or -s to enable shell command execution, use -h or --help to see all options")
+			case "--v", "-verbose":
+				log.Fatal("Please use --verbose or -v to enable verbose output, use -h or --help to see all options")
+			case "-quiet", "--q":
+				log.Fatal("Please use --quiet or -q to enable quiet output, use -h or --help to see all options")
+			case "-help", "--h":
+				log.Fatal("Please use -h or --help to see all options")
 			default:
 				continue
 			}
